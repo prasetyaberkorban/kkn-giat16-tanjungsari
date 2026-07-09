24b0d87ab17d131bf08c9dca9677620005c19df5
Prasetya <prasetyaberkorban@gmail.com>
2026-07-09 13:16:39 +0700

    Auto commit pada 2026-07-09 13:16:39

--- server.js
+++ server.js
@@ -1,33 +1,3 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const connectDB = require('./src/config/db');
-const apiRoutes = require('./src/routes/api');
-const rabRoutes = require('./src/routes/rab');
-
-const app = express();
-
-// Hubungkan ke MongoDB
-connectDB();
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Menyajikan file statis dari folder public
-app.use(express.static(path.join(__dirname, 'public')));
-
-// API Routes
-app.use('/api', apiRoutes);
-app.use('/api/rab', rabRoutes);
-
-// Route fallback untuk frontend (Single Page feel)
-app.get('/absensi', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'absensi.html'));
-});
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
