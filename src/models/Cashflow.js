ddc1f33d673e7c661ec125c18b1333d3967db645
Prasetya <prasetyaberkorban@gmail.com>
2026-07-09 13:47:04 +0700

    Auto commit pada 2026-07-09 13:47:04

--- src/models/Cashflow.js
+++ src/models/Cashflow.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 
 const CashflowSchema = new mongoose.Schema({
   tanggal: { type: String, required: true },
-  keterangan: { type: String, required: true },
+  kategori: { type: String, required: true },
+  deskripsi: { type: String, required: true },
   jenis: { type: String, enum: ['Pemasukan', 'Pengeluaran'], required: true },
+  hargaSatuan: { type: Number, required: true },
+  jumlahBarang: { type: Number, required: true },
   nominal: { type: Number, required: true },
   createdAt: { type: Date, default: Date.now }
 });
