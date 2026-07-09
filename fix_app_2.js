39e35320496cf468e3ff5ecbc59302dd21fc0a88
Prasetya <prasetyaberkorban@gmail.com>
2026-07-09 14:33:12 +0700

    Auto commit pada 2026-07-09 14:33:12

--- fix_app_2.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('public/app.js', 'utf8');
-
-// Remove everything from // Override initial load to fetch cashflow to the end
-content = content.replace(/\/\/ Override initial load to fetch cashflow[\s\S]+/, '');
-
-fs.writeFileSync('public/app.js', content.trim() + '\n');
-console.log('Successfully removed the override block!');
