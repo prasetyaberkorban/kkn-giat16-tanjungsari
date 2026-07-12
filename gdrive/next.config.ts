0b8850509d1dabf1873c6c980e41f870bd6707e8
Prasetya <prasetyaberkorban@gmail.com>
2026-07-12 17:20:00 +0700

    Auto commit pada 2026-07-12 17:20:00

--- gdrive/next.config.ts
+++ gdrive/next.config.ts
@@ -4,10 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   // @ts-ignore
   allowedDevOrigins: ['192.168.1.26'],
-  devIndicators: {
-    appIsrStatus: false,
-    buildActivity: false,
-  },
   basePath: "/gdrive",
 };
 
